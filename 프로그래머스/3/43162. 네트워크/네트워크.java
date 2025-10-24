de70da5aa802a68e79711239def2245060b4aef5
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-24 16:12:44 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void dfs(int node, int[][] computers, boolean[] visited) {
+        visited[node] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[node][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited);
+            }
+        }
+    }
+}
