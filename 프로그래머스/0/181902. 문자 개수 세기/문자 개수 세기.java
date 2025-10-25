0368abe4ee2e1c9be92712ec652698deed7d02fe
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-25 14:23:05 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.09 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+
+        for (char c : my_string.toCharArray()) {
+            if ( 'A' <= c && c <= 'Z') {
+                answer[c - 'A']++;
+            } else if ( 'a' <= c && c <= 'z') {
+                answer[c - 'a' + 26]++;
+            }
+        }
+
+        return answer;
+    }
+}
