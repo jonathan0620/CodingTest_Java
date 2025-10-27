008ea91711cdaaced19762a76b4d8cb5cda4bbdc
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-27 12:42:53 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,27 +1,18 @@
 import java.util.*;
 class Solution {
 	public int[] solution(int[] array, int[][] commands) {
-
+        
         int[] answer = new int[commands.length];
-
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0];
+            int start = commands[i][0] - 1;
             int end = commands[i][1];
-            int k = commands[i][2];
-
-            int size = end - start + 1;
-
-            int[] cutArray = new int[size];
-            int idx = 0;
-            for (int j = start - 1; j < end; j++) {
-                cutArray[idx] = array[j];
-                idx++;
-            }
-
-            Arrays.sort(cutArray);
-
-            answer[i] = cutArray[k - 1];
+            int idx = commands[i][2] - 1;
+            
+            int sliced[] = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(sliced);
+            answer[i] = sliced[idx];
         }
+        
         return answer;
     }
 }
