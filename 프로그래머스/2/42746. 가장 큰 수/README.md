fc1a938e0bda362decdd4f07581cd742845b3902
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-27 13:17:57 +0900

    [level 2] Title: 가장 큰 수, Time: 191.93 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 415 MB, 시간: 3127.10 ms
+메모리: 131 MB, 시간: 191.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 10일 23:29:54
+2025년 10월 27일 13:17:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,32 +1,23 @@
 import java.util.*;
 
 class Solution {
-    static class MyComparator implements Comparator<String> {
-        @Override
-        public int compare(String a, String b) {
-            String ab = a + b;
-            String ba = b + a;
-            return ba.compareTo(ab);
-        }
-    }
 
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
+        
+        String[] strArr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+            strArr[i] = String.valueOf(numbers[i]);
         }
-
-        Arrays.sort(arr, new MyComparator());
-
-        if (arr[0].equals("0")) {
+        
+        Arrays.sort(strArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        String result = String.join("", strArr);
+        
+        if (result.startsWith("0")) {
             return "0";
         }
-
-        String result = "";
-        for (String s : arr) {
-            result += s;
-        }
-
+        
         return result;
+        
     }
 }
