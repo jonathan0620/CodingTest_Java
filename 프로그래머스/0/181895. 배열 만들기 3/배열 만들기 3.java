819defccf8ee21d962cc70d8ebb0e053841979b0
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-28 14:25:24 +0900

    [level 0] Title: 배열 만들기 3, Time: 12.31 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,14 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> list = new ArrayList<>();
         
-        for (int[] range : intervals) {
-            int start = range[0];
-            int end = range[1];
+        // 주어진 구간으로 배열을 잘라서 다 더해서 return
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
             
-            for (int i = start; i <= end; i++) {
-                list.add(arr[i]);
+            for (int k = start; k <= end; k++) {
+                list.add(arr[k]);
             }
         }
         
