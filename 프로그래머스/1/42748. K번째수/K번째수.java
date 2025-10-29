6a3eaf3f9b5751da1dc38d9c67f5b296c25a9696
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-29 15:38:10 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,16 +2,21 @@ import java.util.*;
 class Solution {
 	public int[] solution(int[] array, int[][] commands) {
         
+        // 배열을 i부터 j까지 자르고 k번째 수 구하기.
+        // 배열 자르기 -> 거기서 K번째 수 찾기
         int[] answer = new int[commands.length];
-        for (int i = 0; i < commands.length; i++) {
+        
+        for (int i = 0; i < commands.length; i++) { // commands 수 만큼 돌기
             int start = commands[i][0] - 1;
             int end = commands[i][1];
-            int idx = commands[i][2] - 1;
+            int target = commands[i][2] - 1;
             
             int sliced[] = Arrays.copyOfRange(array, start, end);
             Arrays.sort(sliced);
-            answer[i] = sliced[idx];
+            answer[i] = sliced[target];
+        
         }
+    
         
         return answer;
     }
