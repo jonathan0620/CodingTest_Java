c3b251c44f971b6e10ec1ebb776b9c65eb1a2890
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-29 12:43:15 +0900

    [level 2] Title: 의상, Time: 0.30 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,21 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-
+        // 종류별로 최대 1가지 의상만 착용 가능
+        // 서로 다른 옷의 조합 수 return (int)
+        // ["yellow_hat", "headgear"], 
+        // ["blue_sunglasses", "eyewear"], 
+        // ["green_turban", "headgear"]
+        
         Map<String, Integer> map = new HashMap<>();
-
-        for (String[] c : clothes) {
-            String kind = c[1];
-            map.put(kind, map.getOrDefault(kind, 0) + 1);
+        for (String[] cloth : clothes) {
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
-
+        
+        System.out.println(map);
+        
         int answer = 1;
         for (int count : map.values()) {
             answer *= (count + 1);
         }
-
-        answer = answer-1;
-
-        return answer;
+        return answer - 1;
     }
 }
