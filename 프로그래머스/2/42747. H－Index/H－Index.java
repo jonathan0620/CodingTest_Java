e8f51c0bab33467f51736c23a00e14e5458c55b5
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-29 16:38:51 +0900

    [level 2] Title: H-Index, Time: 2.17 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,15 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int n = citations.length;
+        // n 논문 
+        // h번 이상 인용된 게 h편 이상
+        // 논문의 인용 횟수 배열 citations
+        
         Arrays.sort(citations);
-
+        System.out.print(Arrays.toString(citations));
+        
+        int n = citations.length;
         for (int i = 0; i < n; i++) {
             int answer = n - i;
             if (citations[i] >= answer) {
                 return answer;
             }
         }
+        
         return 0;
     }
 }
