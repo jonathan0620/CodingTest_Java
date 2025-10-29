2406bfe0fd130a67fd0e969398b5d2fab27cefb5
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-29 12:08:02 +0900

    [level 1] Title: 폰켓몬, Time: 6.87 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int pick = nums.length / 2;
-
-        HashSet<Integer> set = new HashSet<>();
+        // 같은 종류의 폰켓몬은 같은 번호
+        // 가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아, 
+        // 그때의 폰켓몬 종류 번호의 개수를 return 
+        
+        // N 마리 중 N / 2 마리 골라야 함
+        // 최대 고를 수 있는 폰켓몬의 종류
+        
+        // set으로 중복 제거?
+        Set<Integer> set = new HashSet<>();
         for (int n : nums) {
             set.add(n);
         }
 
-        return Math.min(set.size(), pick);
+        int len = nums.length;
+        
+        int setLen = set.size(); // set 사이즈
+        int maxi = len / 2; // N / 2
+        int answer = Math.min(maxi, setLen);
+        
+        return answer;
     }
 }
