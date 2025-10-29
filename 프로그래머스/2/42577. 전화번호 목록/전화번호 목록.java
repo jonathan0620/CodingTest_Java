a815b79c91dd8592ea87b7fc01a3f75081b33251
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-10-29 12:21:31 +0900

    [level 2] Title: 전화번호 목록, Time: 304.72 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,17 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
-
-        for (String number : phone_book) {
-            for (int end = 1; end < number.length(); end++) {
-                String prefix = number.substring(0, end);
-                if (set.contains(prefix)) {
-                    return false;
-                }
+        // 다른 번호의 접두어 인가?
+        // 전화번호 북 phone_book
+        
+        Arrays.sort(phone_book); // 사전 순 정렬
+        
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
+        
         return true;
     }
 }
