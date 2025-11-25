d871dc7cd1036518fec9c921744b56a550b30e71
Jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-11-25 19:46:44 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.48 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[k];
+        Arrays.fill(answer, -1);
+
+        Set<Integer> set = new HashSet<>();
+        int idx = 0;
+
+        for (int num : arr) {
+            if (set.contains(num)) continue;
+
+            set.add(num);
+            answer[idx] = num;
+            idx++;
+
+            if (idx == k) break;
+        }
+        return answer;
+    }
+}
